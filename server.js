cbab046078879c08c44bc8218463cba0a9875c09
Lyefyre <v.schaer@gmx.ch>
2020-05-02 12:08:11 +0200

    deploy heroku

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/blk-design-system-angular-master'));
+app.use(express.static(__dirname + '/dist/blk-design-system-angular'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/blk-design-system-angular-master/src/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/blk-design-system-angular/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080, function () {
